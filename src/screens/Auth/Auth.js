4175fe4bf72451089ed9d8b20e6664dfaa9081b8
a96lex <a96lex@gmail.com>
2021-07-15 19:28:31 +0200

    add context action

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,11 +1,16 @@
 import React, { useState } from "react";
 import { Text, TouchableOpacity, View } from "react-native";
+import { useUserActions } from "../../context/UserContext";
 
 export function Auth() {
   const [isLogin, setIsLogin] = useState(true);
+  const { signIn } = useUserActions();
   return (
-    <View style={{ flex: 1 }}>
+    <View>
       <Text>{isLogin ? "Haz login en la app" : "Regístrate en la app"}</Text>
+      <TouchableOpacity onPress={() => signIn()}>
+        <Text>Hcer login falso</Text>
+      </TouchableOpacity>
       <TouchableOpacity onPress={() => setIsLogin(!isLogin)}>
         <Text>
           {isLogin
