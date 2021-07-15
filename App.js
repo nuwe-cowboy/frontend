e923687802b9374485712407613005014c45a420
a96lex <a96lex@gmail.com>
2021-07-15 12:01:06 +0200

    conditional rendering of Auth screen

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+
+import { useUserState } from "./src/context/UserContext";
+import { Auth } from "./src/screens";
+import { Text, TouchableOpacity, View } from "react-native";
 
 export default function App() {
+  const { loggedIn } = useUserState();
   return (
     <View style={styles.container}>
       <TouchableOpacity onPress={() => console.log("alex")}>
         <Text>Alex</Text>
+        {loggedIn ? <Text>Logged In deuna</Text> : <Auth />}
       </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
