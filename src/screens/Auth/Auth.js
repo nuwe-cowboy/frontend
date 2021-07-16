ba91362fde530fac7db23acd7119d6128ff081aa
a96lex <a96lex@gmail.com>
2021-07-16 17:31:54 +0200

    updated auth page

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,23 +1,82 @@
 import React, { useState } from "react";
-import { Text, TouchableOpacity, View } from "react-native";
+import { Text, TouchableOpacity, Image, View } from "react-native";
 import { useUserActions } from "../../context/UserContext";
+import {
+  AuthButton,
+  AuthContainer,
+  AuthInput,
+  AuthLogo,
+  HeaderText,
+} from "./Auth.styled";
 
 export function Auth() {
   const [isLogin, setIsLogin] = useState(true);
   const { signIn } = useUserActions();
+  const [name, setName] = useState("");
+  const [surname, setSurame] = useState("");
+  const [mail, setMail] = useState("");
+  const [password, setPassword] = useState("");
   return (
-    <View>
-      <Text>{isLogin ? "Haz login en la app" : "Regístrate en la app"}</Text>
-      <TouchableOpacity onPress={() => signIn()}>
-        <Text>Hcer login falso</Text>
-      </TouchableOpacity>
+    <AuthContainer>
+      <View style={{ flex: 1 }}>
+        <AuthLogo source={require("../../assets/logo_wide.png")} />
+      </View>
+      <View style={{ flex: 5 }}>
+        <HeaderText>
+          {isLogin ? "Haz login en la app" : "Regístrate en la app"}
+        </HeaderText>
+        {!isLogin && (
+          <>
+            <FormFragment
+              label="Nombre"
+              placeholder="Introduce tu nombre"
+              value={surname}
+              onChangeText={setSurame}
+            />
+            <FormFragment
+              label="Apellidos"
+              placeholder="Introduce tus apellidos"
+              value={name}
+              onChangeText={setName}
+            />
+          </>
+        )}
+        <FormFragment
+          label="Mail"
+          placeholder="Introduce tu mail"
+          value={mail}
+          onChangeText={setMail}
+        />
+        <FormFragment
+          label="Contraseña"
+          placeholder="Introduce tu contraseña"
+          value={password}
+          onChangeText={setPassword}
+        />
+      </View>
+      <AuthButton onPress={() => signIn()}>
+        <Text>Hacer login falso</Text>
+      </AuthButton>
       <TouchableOpacity onPress={() => setIsLogin(!isLogin)}>
         <Text>
           {isLogin
-            ? "Ya tengo cuenta, ir a login"
-            : "No tengo cuenta, ir a registro"}
+            ? "No tengo cuenta, ir a registro"
+            : "Ya tengo cuenta, ir a login"}
         </Text>
       </TouchableOpacity>
-    </View>
+    </AuthContainer>
+  );
+}
+
+function FormFragment({ label, placeholder, value, onChangeText }) {
+  return (
+    <>
+      <Text>{label}</Text>
+      <AuthInput
+        placeholder={placeholder}
+        value={value}
+        onChangeText={(t) => onChangeText(t)}
+      />
+    </>
   );
 }
