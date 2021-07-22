aeffc7d00d4382b80f9c9c6177ba6e36c2695805
a96lex <a96lex@gmail.com>
2021-07-22 18:21:15 +0200

    removed auth action

--- src/screens/Main/Main.js
+++ src/screens/Main/Main.js
@@ -1,17 +1,11 @@
 import React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
 import { Events } from "../../components";
-import { useUserActions } from "../../context/UserContext";
 import { MainContainer } from "./Main.styled";
 
 export const Main = () => {
-  const { logOff } = useUserActions();
   return (
     <MainContainer>
       <Events />
-      <TouchableOpacity onPress={() => logOff()}>
-        <Text>Salir</Text>
-      </TouchableOpacity>
     </MainContainer>
   );
 };
