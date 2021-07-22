f329f58f28c12e5e98d313f16173045f4d9bae21
a96lex <a96lex@gmail.com>
2021-07-22 17:40:21 +0200

    add navigation

--- App.js
+++ App.js
@@ -3,13 +3,27 @@ import { StatusBar } from "expo-status-bar";
 
 import { View } from "react-native";
 import { useUserState } from "./src/context/UserContext";
-import { Auth, Main } from "./src/screens";
+import { Auth, Main, Newsletter } from "./src/screens";
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
-  const { loggedIn } = useUserState();
+  const { loggedIn, authRequested } = useUserState();
   return (
     <View style={{ height: "100vh", width: "100vw" }}>
-      {loggedIn ? <Main /> : <Auth />}
+      {authRequested && <Auth />}
+
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Eventos" component={Main} />
+          {loggedIn && <Tab.Screen name="Mis Eventos" component={Main} />}
+          <Tab.Screen name="Newsletter" component={Newsletter} />
+        </Tab.Navigator>
+      </NavigationContainer>
+
       <StatusBar style="auto" />
     </View>
   );
