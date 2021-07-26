8c7992109b71493b53c0262601c1adaa992c12a3
cristina <cristina.outeda.rua@gmail.com>
2021-07-26 19:39:00 +0200

    reStyled modal

--- src/screens/Auth/Auth.styled.js
+++ src/screens/Auth/Auth.styled.js
@@ -8,7 +8,6 @@ export const AuthBg = styled.View`
   width: 100vw;
   height: 100vh;
   background: rgba(0, 0, 0, 0.2);
-  flex: 1;
   z-index: 100;
   align-items: center;
   justify-content: center;
@@ -19,14 +18,19 @@ export const AuthContainer = styled.View`
   align-items: center;
   background: ${Theme.background};
   width: 90%;
+  max-width: 800px;
   border-radius: 15px;
   height: 80%;
   justify-content: space-around;
-  padding: 0 1rem;
+  padding: 2rem 1rem;
 `;
 
 export const AuthFormContainer = styled.View`
-  width: 100%;
+  width: 70%;
+  display: flex;
+  align-items: center;
+  justify-content: start;
+  height: 60%;
 `;
 
 export const HeaderText = styled.Text`
@@ -38,28 +42,24 @@ export const HeaderText = styled.Text`
 
 export const AuthInput = styled.TextInput`
   width: 100%;
+  max-width: 300px;
   height: 35px;
   padding: 4px;
   border-width: 0.5px;
   border-radius: 5px;
-  margin-bottom: 6px;
   border-color: ${Theme.borderInput};
 `;
 
 export const AuthButton = styled.TouchableOpacity`
-  width: 100%;
+  width: 120px;
   background: ${Theme.primary};
   border-width: 2px;
   padding: 10px;
   border-radius: 5px;
   border-color: ${Theme.secondary};
-  bottom: 15%;
-  position: absolute;
-`;
-
-export const TextButton = styled.Text`
   text-align: center;
   color: #fff;
   font-variant: small-caps;
   font-weight: 600;
+  font-size: 24px;
 `;
