1a14fc9d42b084339e34dee434598f8bf8080a7a
a96lex <a96lex@gmail.com>
2021-07-28 17:07:08 +0200

    add simple card component

---
+++ src/components/ANuweComponents/Card/Card.styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components/native";
+import { Theme } from "../../../helpers";
+
+export const StyledCard = styled.View`
+  padding: 1rem;
+  width: 100%;
+  margin: 0.5rem 0;
+  border-radius: 5px;
+  background: ${Theme.background};
+`;
