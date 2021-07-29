f3fc86958ef3871e4dce6bcf42fc6ac2dbaa919b
a96lex <a96lex@gmail.com>
2021-07-29 18:51:49 +0200

    responsive

--- src/components/NuweComponents/NCard/NCard.styled.js
+++ src/components/NuweComponents/NCard/NCard.styled.js
@@ -3,8 +3,10 @@ import { Theme } from "../../../helpers";
 
 export const StyledCard = styled.View`
   padding: 1rem;
-  width: 100%;
-  margin: 0.5rem 0;
+  min-width: 400px;
+  max-width: 600px;
+  margin: 0.5rem;
   border-radius: 5px;
+  flex-grow: 1;
   background: ${Theme.background};
 `;
